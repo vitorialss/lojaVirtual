af2fc001f6612de2d6cfddc15af2fdf2219157cc
vitorialss <107154247+vitorialss@users.noreply.github.com>
2023-07-17 15:32:57 -0300

    "typescript e style c. adicionados"

---
+++ lojaVirtual/src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <span>Soso</span>
+      <ul>
+        <li>
+          <a>Saúde diária</a>
+        </li>
+        <li>
+          <a>Perda de peso</a>
+        </li>
+        <li>
+          <a>Pele</a>
+        </li>
+        <li>
+          <a>Cabelo</a>
+        </li>
+        <li>
+          <a>Blogs</a>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <a></a>
+        </li>
+        <li>
+          <a></a>
+        </li>
+        <li>
+          <a></a>
+        </li>
+      </ul>
+      <button>Faça o teste</button>
+    </nav>
+  );
+}
